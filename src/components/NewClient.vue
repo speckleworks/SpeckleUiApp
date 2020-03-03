a5db375a0c15b0f1f377bb338c0d95568adcfbad
matteo.cominetti <matteo.cominetti@arup.com>
2020-03-03 09:40:09 +0000

    formatting

--- src/components/NewClient.vue
+++ src/components/NewClient.vue
@@ -34,7 +34,14 @@
             <p
               class="caption"
             >If the stream you're looking for doesn't show up here, try refreshing the list and make sure it's shared with you!</p>
-            <v-text-field v-model="search" label="Search streams" prepend-icon="search" clearable  @click:append="refreshStreamsAtAccount()" append-icon="refresh"></v-text-field>
+            <v-text-field
+              v-model="search"
+              label="Search streams"
+              prepend-icon="search"
+              clearable
+              @click:append="refreshStreamsAtAccount()"
+              append-icon="refresh"
+            ></v-text-field>
             <v-list max-height="200" class="overflow-y-auto">
               <v-list-item-group color="primary" v-model="selectedStream">
                 <v-list-item
